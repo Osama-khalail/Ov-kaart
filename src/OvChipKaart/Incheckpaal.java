7503540e04e1536b0c4a882bfb189f8ccac63dd8
Osso <a.asamaklil500@GMAIL.COM>
2023-04-17 11:54:20 +0200

    .

--- src/OvChipKaart/Incheckpaal.java
+++ src/OvChipKaart/Incheckpaal.java
@@ -1,19 +1,33 @@
-package OvChipKaart;
-import java.util.Date;
+import java.util.Scanner;
 
-public class Incheckpaal {
-	public void inchecken(OVChipkaart  ovChipkaart) {
-		double instaptarief = 20.00;
-		double saldo = ovChipkaart.getSaldo();
-		
-		if(saldo < instaptarief) {
-			System.out.println("Saldo onvoldoende. U kant helaas niet inchecken.");
-		}else {
-			ovChipkaart.setSaldo(saldo - instaptarief);
-			System.out.println("inchecken is gelukt");
-		}
-		
-		
-	}
-
+public class IncheckenUI {
+    public static void main(String[] args) {
+        // Maak nieuwe reiziger en OV-chipkaart aan
+        Reiziger reiziger = new Reiziger("Henk", "de Vries", "01-01-2000");
+        OVChipkaart ovChipkaart = new OVChipkaart(123456789, 50.0, reiziger);
+        
+        // Maak nieuwe incheckpaal aan en stel OV-chipkaart in
+        Incheckpaal incheckpaal = new Incheckpaal();
+        incheckpaal.setOVChipkaart(ovChipkaart);
+        
+        // Voer incheckproces uit
+        inchecken(incheckpaal);
+    }
+    
+    public static void inchecken(Incheckpaal incheckpaal) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Wilt u inchecken of uitchecken? (I/U)");
+        String inOfUit = input.nextLine();
+        
+        if (inOfUit.equalsIgnoreCase("I")) {
+            // Voer incheckproces uit
+            incheckpaal.inchecken();
+        } else if (inOfUit.equalsIgnoreCase("U")) {
+            // Voer uitcheckproces uit
+            incheckpaal.uitchecken();
+        } else {
+            System.out.println("Ongeldige invoer. Probeer het opnieuw.");
+            inchecken(incheckpaal);
+        }
+    }
 }
